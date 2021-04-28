e4c6f96f66a8ea54163ec73757b15bda6668008c
mh3mh0 <jonxca@icloud.com>
2021-04-28 13:09:49 +0200

    routes2

--- src/pages/Character.js
+++ src/pages/Character.js
@@ -1,17 +1,17 @@
 const Character = () => {
   const view = `
-        <div class="Character-inner">
-            <article class="Character-card">
+        <div class="Characters-inner">
+            <article class="Characters-card">
                 <img src="image" alt="name" />
                 <h2>Name</h2>
             </article>
 
-            <article class="Character-card">
+            <article class="Characters-card">
                 <h3>Episodes:</h3>
                 <h3>Status:</h3>
                 <h3>Species:</h3>
-                <h3>Gender</h3>
-                <h3>origin:</h3>
+                <h3>Gender:</h3>
+                <h3>Origin:</h3>
                 <h3>Last Location:</h3>
             </article>
         </div>
